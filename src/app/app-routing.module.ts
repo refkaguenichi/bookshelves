8a27ced997e187195fffe458c130eeedf54e5f96
Refka Guenichi <refkaguenichi@gmail.com>
2022-10-30 21:01:58 +0100

    authentication

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { SignupComponent } from './auth/signup/signup.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { BookListComponent } from './book-list/book-list.component';
+import { SingleBookComponent } from './book-list/single-book/single-book.component';
+import { BookFormComponent } from './book-list/book-form/book-form.component';
+
+const routes: Routes = [
+  { path: 'auth/signup', component: SignupComponent },
+  { path: 'auth/signin', component: SigninComponent },
+  { path: 'books', component: BookListComponent },
+  { path: 'books/view/:id', component: SingleBookComponent },
+  { path: 'books/new', component: BookFormComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/database';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bookshelves';
+  constructor(){
+    const firebaseConfig = {
+      apiKey: 'AIzaSyBlKDAPeeCBi3EwO0pEf0G1X7NTFHW4hRQ',
+      authDomain: 'bookshelves-c8dff.firebaseapp.com',
+      projectId: 'bookshelves-c8dff',
+      storageBucket: 'bookshelves-c8dff.appspot.com',
+      messagingSenderId: '743215954455',
+      appId: '1:743215954455:web:9a7c5f77355b4da0de3a52',
+      measurementId: 'G-5S37MM347W',
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+  }
 }
